22786f92a23a26bb44601dc6097df03ec826382f
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-17 00:00:00 -0600

    Automated TFS Import Comment: Script created  (Changeset=782390)

---
+++ src/P/Build.bat
@@ -0,0 +1,15 @@
+
+set scritpToExecute=%1
+set remoteSigningVM=%2
+set remoteSignigScript=%3
+set user=%4
+set password=%5
+
+
+REM Compile Solutions
+REm Determine where the build outputs are
+
+set outputBinaries='\\10.210.0.184\XP2Build.17.2\Staging\Client\XP2'
+
+
+powershell "%scritpToExecute% %RemoteSigningVM% %remoteSignigScript%  %outputBinaries% %user% %password%"
