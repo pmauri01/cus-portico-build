065a23ada606b5dc7404a347df336da6fa576f1d
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-20 00:00:00 -0600

    Automated TFS Import Comment: sa  (Changeset=782627)

--- src/P/BuildGalaxy.bat
+++ src/P/BuildGalaxy.bat
@@ -9,7 +9,8 @@ set password=%5
 REM Compile Solutions
 REm Determine where the build outputs are
 
-set outputBinaries="\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1"
+set outputBinaries="'\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1'"
 
+echo outputBinaries
 
 powershell "%scritpToExecute% %RemoteSigningVM% %remoteSignigScript%  %outputBinaries% %user% %password%"
