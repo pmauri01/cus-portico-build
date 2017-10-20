46ae85e89c41085092e65f281c6339c3d05c9499
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-20 00:00:00 -0600

    Automated TFS Import Comment: test for galazy  (Changeset=782624)

---
+++ src/P/BuildGalaxy.bat
@@ -0,0 +1,15 @@
+
+set scritpToExecute=%1
+set remoteSigningVM=%2
+set remoteSignigScript=%3
+set user=%4
+set password=%5
+
+
+REM Compile Solutions
+REm Determine where the build outputs are
+
+set outputBinaries='\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1'
+
+
+powershell "%scritpToExecute% %RemoteSigningVM% %remoteSignigScript%  %outputBinaries% %user% %password%"
