33976886cdf161dff2e82c3e6634d487cfd0a735
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-20 00:00:00 -0600

    Automated TFS Import Comment: sa  (Changeset=782686)

--- src/P/BuildGalaxy.bat
+++ src/P/BuildGalaxy.bat
@@ -10,7 +10,7 @@ set password=%5
 REM Compile Solutions
 REm Determine where the build outputs are
 
-set outputBinaries='\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1'
+set outputBinaries=\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1
 
 echo outputBinaries %outputBinaries%
 
