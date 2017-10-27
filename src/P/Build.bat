bd2bc53e97de53b4f7735ff6d3cd06ee37b433e5
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-27 00:00:00 -0600

    Automated TFS Import Comment: Test  (Changeset=783226)

--- src/P/Build.bat
+++ src/P/Build.bat
@@ -2,8 +2,8 @@
 set scritpToExecute=%1
 set remoteSigningVM=%2
 set remoteSignigScript=%3
-set user=%4
-set password=%5
+rem set user=%4
+rem set password=%5
 
 
 REM Compile Solutions
@@ -12,4 +12,4 @@ REm Determine where the build outputs are
 set outputBinaries='\\10.210.0.184\XP2Build.17.2\Staging\Client\XP2'
 
 
-powershell "%scritpToExecute% %RemoteSigningVM% %remoteSignigScript%  %outputBinaries% %user% %password%"
+powershell "%scritpToExecute% %RemoteSigningVM% %remoteSignigScript%  %outputBinaries% '%user%' '%password%'"
