331fec271b62f6df316317ecb245046f8d369126
Pablo Isaac Mauricio <FISERVCUS\pmauricio>
2017-10-31 00:00:00 -0600

    Automated TFS Import Comment: Added share  (Changeset=783461)

--- src/P/Build.bat
+++ src/P/Build.bat
@@ -10,7 +10,15 @@ ECHO user: %user%
 REM Compile Solutions
 REm Determine where the build outputs are
 
-set outputBinaries='\\10.210.0.184\XP2Build.17.2\Staging\Client\XP2'
+REM binaries is XP2 valid machine
+REM set outputBinaries='\\10.210.0.184\XP2Build.17.2\Staging\Client\XP2'
+
+REM binaries is DataSafe valid machine
+set outputBinaries='\\VMTFSBUILD2\Release'
+
+REM binaries is Galaxy valid machine
+Rem set outputBinaries='\\trovd000174\TfsDrop\tfsUIDeploy\R7 - UI Main Build\R7 - UI Main Build_2.1.07.00_20170914.1'
+REM set outputBinaries=%outputBinaries: =SPACE%
 
 
 powershell "%scritpToExecute% %RemoteSigningVM% %RemoteSigningScript%  %outputBinaries% %user% %password%"
